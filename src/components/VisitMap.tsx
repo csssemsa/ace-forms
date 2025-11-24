2bebc3cd289dc4b5c8801a4faf37fb795dded1fc
codealixjr <csssemsa@gmail.com>
2025-11-23 22:42:08 -0300

    fix: otimizar carregamento de icones do mapa

--- src/components/VisitMap.tsx
+++ src/components/VisitMap.tsx
@@ -43,6 +43,25 @@ function MapBounds({ points }: { points: GeocodedVisit[] }) {
     return null;
 }
 
+// Ícones personalizados
+const domiciliarIcon = new Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+
+const vetorialIcon = new Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-gold.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+
 export const VisitMap: React.FC<VisitMapProps> = ({ user }) => {
     const [geocodedVisits, setGeocodedVisits] = useState<GeocodedVisit[]>([]);
     const [loading, setLoading] = useState(true);
@@ -65,25 +84,6 @@ export const VisitMap: React.FC<VisitMapProps> = ({ user }) => {
     const [showProfessionalDropdown, setShowProfessionalDropdown] = useState(false);
 
     const defaultCenter: [number, number] = [-6.0675, -50.0356];
-
-    // Ícones personalizados
-    const domiciliarIcon = new Icon({
-        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
-        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
-        iconSize: [25, 41],
-        iconAnchor: [12, 41],
-        popupAnchor: [1, -34],
-        shadowSize: [41, 41]
-    });
-
-    const vetorialIcon = new Icon({
-        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-gold.png',
-        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
-        iconSize: [25, 41],
-        iconAnchor: [12, 41],
-        popupAnchor: [1, -34],
-        shadowSize: [41, 41]
-    });
 
     useEffect(() => {
         if (selectedProfessional === 'all') {
